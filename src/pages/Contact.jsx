eeed76f2baa8b86734b0313b3f37d9576ae55489
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2024-03-28 13:00:07 -0300

    form warnings included

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -176,12 +176,118 @@
 
 import { BsWhatsapp, BsFillEnvelopeFill } from "react-icons/bs";
 import { Helmet } from "react-helmet-async";
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
-
   const form = useRef();
+
+  const phoneRegEx = /^\+?[1-9][0-9]{7,14}$/;
+  const emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [inputPhone, setInputPhone] = useState("");
+  const [inputName, setInputName] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
+
+  const [phoneError, setPhoneError] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const [submitMessage, setSubmitMessage] = useState("");
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+
+  // Cargo del localStorage los valores de los inputs
+  useEffect(() => {
+    const storedPhone = localStorage.getItem("phone");
+    const storedName = localStorage.getItem("name");
+    const storedEmail = localStorage.getItem("email");
+
+    if (storedPhone) {
+      setInputPhone(JSON.parse(storedPhone));
+      validatePhone(JSON.parse(storedPhone));
+    }
+
+    if (storedName) {
+      setName(JSON.parse(storedName));
+      setInputName(JSON.parse(storedName));
+    }
+
+    if (storedEmail) {
+      setInputEmail(JSON.parse(storedEmail));
+      validateEmail(JSON.parse(storedEmail));
+    }
+  }, []);
+
+  // Chequeo si el input de phone,name o email estan vacíos y en ese caso desactivo el boton submit del form
+  useEffect(() => {
+    if (phone === "" || name === "" || email === "") {
+      setSubmitDisabled(true);
+    } else {
+      setSubmitDisabled(false);
+    }
+  });
+
+  // Chequeo si phoneError tiene algun valor y de ser así desactivo el boton submit del form
+  useEffect(() => {
+    if (phoneError !== "" || emailError !== "") {
+      setSubmitDisabled(true);
+    } else {
+      setSubmitDisabled(false);
+    }
+  }, [phoneError, emailError]);
+
+  // Muestro un mensaje para completar los campos vacíos
+  useEffect(() => {
+    if (submitDisabled === true) {
+      setSubmitMessage("Debes completar los campos");
+    } else {
+      setSubmitMessage("");
+    }
+  }, [submitDisabled]);
+
+  // funcion para guardar el valor de los input en localStorage
+  const storeInputPhone = (valor) => {
+    setInputPhone(valor);
+    localStorage.setItem("phone", JSON.stringify(valor));
+  };
+  const storeInputName = (valor) => {
+    setInputName(valor);
+    localStorage.setItem("name", JSON.stringify(valor));
+  };
+  const storeInputEmail = (valor) => {
+    setInputEmail(valor);
+    localStorage.setItem("email", JSON.stringify(valor));
+  };
+
+  // Funcion para validar el valor phone con el RegEx de telefono declarado previamente ("phoneRegEx")
+  const validatePhone = (value) => {
+    if (value === "") {
+      setPhone(value);
+      setPhoneError("");
+    } else if (String(value).toLowerCase().match(phoneRegEx)) {
+      setPhone(value);
+      setPhoneError("");
+    } else {
+      setPhoneError("Teléfono inválido");
+    }
+  };
+
+  // Funcion para validar el valor email con el RegEx de email declarado previamente ("EmailRegEx")
+
+  const validateEmail = (value) => {
+    if (value === "") {
+      setEmail(value);
+      setEmailError("");
+    } else if (String(value).toLowerCase().match(emailRegEx)) {
+      setEmail(value);
+      setEmailError("");
+    } else {
+      setEmailError("Correo electrónico inválido");
+    }
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -193,7 +299,7 @@ export const Contact = () => {
       .then(
         () => {
           console.log("SUCCESS!");
-          e.target.reset()
+          e.target.reset();
         },
         (error) => {
           console.log("FAILED...", error.text);
@@ -224,14 +330,59 @@ export const Contact = () => {
                   </div>
                 </div>
                 <div className="fondoVerdeC">
-                  <form className="mx-4 py-3 px-3" ref={form} onSubmit={sendEmail}>
-                    <label>Name</label>
-                    <input type="text" name="user_name" />
-                    <label>Email</label>
-                    <input type="email" name="user_email" />
-                    <label>Message</label>
+                  <form
+                    className="d-flex flex-column justify-content-evenly mx-4 py-3 px-3"
+                    ref={form}
+                    onSubmit={sendEmail}
+                  >
+                    <label>Nombre *</label>
+                    <input
+                      name="user_name"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                        storeInputName(e.target.value);
+                      }}
+                      onBlur={(e) => {
+                        setName(e.target.value);
+                        storeInputName(e.target.value);
+                      }}
+                      defaultValue={inputName}
+                    />
+                    <label>Email *</label>
+                    <input
+                      name="user_email"
+                      onChange={(e) => {
+                        validateEmail(e.target.value);
+                        storeInputEmail(e.target.value);
+                      }}
+                      onBlur={(e) => {
+                        validateEmail(e.target.value);
+                        storeInputEmail(e.target.value);
+                      }}
+                      defaultValue={inputEmail}
+                    />
+                    <div className="mt-2 text-danger">{emailError}</div>
+                    <label>Teléfono *</label>
+                    <input
+                      name="user_phone"
+                      onChange={(e) => {
+                        validatePhone(e.target.value);
+                        storeInputPhone(e.target.value);
+                      }}
+                      onBlur={(e) => {
+                        validatePhone(e.target.value);
+                        storeInputPhone(e.target.value);
+                      }}
+                      defaultValue={inputPhone}
+                    />
+                    <div className="mt-2 text-danger">{phoneError}</div>
+                    <label>Motivo de Consulta (Opcional)</label>
                     <textarea name="message" />
-                    <input type="submit" value="Send" />
+                    <input
+                      type="submit"
+                      value="Enviar consulta"
+                      disabled={submitDisabled}
+                    />
                   </form>
                 </div>
               </div>
@@ -286,14 +437,59 @@ export const Contact = () => {
                   </div>
                 </div>
                 <div className="fondoVerdeC">
-                  <form className="mx-4 py-3 px-3" ref={form} onSubmit={sendEmail}>
-                    <label>Name</label>
-                    <input type="text" name="user_name" />
-                    <label>Email</label>
-                    <input type="email" name="user_email" />
-                    <label>Message</label>
+                  <form
+                    className="d-flex flex-column justify-content-evenly mx-4 py-3 px-3"
+                    ref={form}
+                    onSubmit={sendEmail}
+                  >
+                    <label>Nombre *</label>
+                    <input
+                      name="user_name"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                        storeInputName(e.target.value);
+                      }}
+                      onBlur={(e) => {
+                        setName(e.target.value);
+                        storeInputName(e.target.value);
+                      }}
+                      defaultValue={inputName}
+                    />
+                    <label>Email *</label>
+                    <input
+                      name="user_email"
+                      onChange={(e) => {
+                        validateEmail(e.target.value);
+                        storeInputEmail(e.target.value);
+                      }}
+                      onBlur={(e) => {
+                        validateEmail(e.target.value);
+                        storeInputEmail(e.target.value);
+                      }}
+                      defaultValue={inputEmail}
+                    />
+                    <div className="mt-2 text-danger">{emailError}</div>
+                    <label>Teléfono *</label>
+                    <input
+                      name="user_phone"
+                      onChange={(e) => {
+                        validatePhone(e.target.value);
+                        storeInputPhone(e.target.value);
+                      }}
+                      onBlur={(e) => {
+                        validatePhone(e.target.value);
+                        storeInputPhone(e.target.value);
+                      }}
+                      defaultValue={inputPhone}
+                    />
+                    <div className="mt-2 text-danger">{phoneError}</div>
+                    <label>Motivo de Consulta (Opcional)</label>
                     <textarea name="message" />
-                    <input type="submit" value="Send" />
+                    <input
+                      type="submit"
+                      value="Enviar consulta"
+                      disabled={submitDisabled}
+                    />
                   </form>
                 </div>
               </div>
